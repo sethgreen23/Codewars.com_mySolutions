fbb98eda2c9c2211ea7ea91aacc9fa5fa939d0f9
Michael Holley <47337897+MichaelHolley@users.noreply.github.com>
2019-02-15 11:50:08 +0100

    Update 6kyu - GrowthDurationTillTarget.py

--- Python 3/6kyu - GrowthDurationTillTarget.py
+++ Python 3/6kyu - GrowthDurationTillTarget.py
@@ -1,6 +1,22 @@
-def nb_year(p0, percent, aug, p):
-    p0 = p0 + p0 * percent/100
-    p0 += aug
-    if(p0 >= p):
-        return 1
-    return nb_year(p0, percent, aug, p) + 1
+def solution(number):
+    result = []
+    
+    list3 = (multipleTillBorder(3, number))
+    result += list3
+
+    list5 = (multipleTillBorder(5, number))
+    for x in list5:
+        if x not in result:
+            result.append(x)
+
+    result.sort()
+    return sum(result)
+    
+
+def multipleTillBorder(x , b):
+    result = []
+    i = 0
+    while x * (i + 1) < b:
+        result.append(x * (i + 1))
+        i += 1
+    return result
