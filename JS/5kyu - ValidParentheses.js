88f1a010521a954c6416e42e5b952376ab178ef3
Michael Holley <47337897+MichaelHolley@users.noreply.github.com>
2019-02-19 14:04:44 +0100

    Add files via upload

---
+++ JS/5kyu - ValidParentheses.js
@@ -0,0 +1,7 @@
+function validParentheses(parens){
+    var length = parens.length / 2;
+    for (var i = 0; i <= length; ++i) {
+      parens = parens.replace('()', '');
+    }
+    return parens == '';
+}
