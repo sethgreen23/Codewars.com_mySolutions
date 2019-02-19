de6b494ca28c3794430ca4cb7a7712d327baafbb
Michael Holley <47337897+MichaelHolley@users.noreply.github.com>
2019-02-19 13:06:58 +0100

    Update 8kyu - OddOrEven.js

--- JS/8kyu - OddOrEven.js
+++ JS/8kyu - OddOrEven.js
@@ -3,4 +3,8 @@ function even_or_odd(number) {
         return "Odd";
     else
         return "Even";
-  }
+}
+
+function even_or_oddAlternative(number) {
+  return Math.abs(number) % 2 == 1 ? "Odd" : "Even"
+}
